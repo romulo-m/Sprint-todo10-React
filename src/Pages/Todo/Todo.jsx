87b76780b733e129ee4ec8e0d860ff492b0f5f95
Broad <romulo.mgaudencio@gmail.com>
2022-08-26 09:31:18 -0300

    instalação de fontes

---
+++ src/Pages/Todo/Todo.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Form from '../../components/Form/Form'
+import Aside from '../../components/Aside/Aside'
+import S from './Todo.module.css'
+
+
+const Todo = () => {
+  return (
+    <main className={S.main}>
+      <Aside />
+      {/* <Form /> */}
+    </main>
+    
+  )
+}
+
+export default Todo
